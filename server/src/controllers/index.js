9b51c31da1b0ded4c7ad964d61ccc7964bf48f2b
庄清惠 <zhuangqhc@163.com>
2016-11-04 23:28:01 +0800

    serve static file

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -1,5 +1,7 @@
 const Router = require('koa-router');
 const compose = require('koa-compose');
+const serve = require('koa-static');
+const path = require('path');
 const userCtrl = require('./user');
 
 const router = new Router();
@@ -7,6 +9,7 @@ const router = new Router();
 userCtrl(router);
 
 module.exports = () => compose([
+  serve(path.join(__dirname, '..', '..', '..', 'public')),
   router.routes(),
   router.allowedMethods(),
 ]);
