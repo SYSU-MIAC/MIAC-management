94c1edc38e6ef4d2774c0f2757b4cb1a878622ed
mensu <yxshw55@qq.com>
2017-01-30 18:55:19 +0800

    同步user的model

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -27,6 +27,22 @@ const schema = {
   email: {   // E-mail
     type: String,
     match: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
+    required: true,
+  },
+  hw: {
+    type: [{  // Homeworks
+      title: String,  // Homework's title
+      file: String,  // path to the file
+      comment: {  // comments
+        text: String,  // comment's content
+        author: String,  // who comment
+      },
+    }],
+    default: [],
+  },
+  deleted: {
+    type: Boolean,
+    default: false,
   },
 };
 
