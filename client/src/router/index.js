bcc036e59a1ac7e042638fcdf07ba493060b9655
MegrezZhu <mystery490815101@gmail.com>
2017-02-22 16:24:18 +0800

    增加了仿ElementUI的走马灯组件（图片轮播）

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,15 +3,17 @@ import Router from 'vue-router'
 import ArticleView from '../views/ArticleView.vue'
 import ProfileView from '../views/ProfileView.vue'
 import HomeworkView from '../views/HomeworkView.vue'
+import IndexView from '../views/IndexView.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  scrollBehavior: () => ({ y: 0 }),
+  scrollBehavior: () => ({y: 0}),
   routes: [
-    { path: '/', component: ArticleView },
-    { path: '/hw', component: HomeworkView },
-    { path: '/profile', component: ProfileView }
+    {path: '/', component: ArticleView},
+    {path: '/hw', component: HomeworkView},
+    {path: '/profile', component: ProfileView},
+    {path: '/index', component: IndexView}
   ]
 })
